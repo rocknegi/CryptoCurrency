58e5d79268d28c965d7d4d3482355cd11d8fae53
rocknegi <rocknegi53@gmail.com>
2018-04-30 21:37:41 +0530

    Revert "Beautify"
    This reverts commit ccb66c89ece3cd649a6a693e3ed9cb373b5faa1a.

--- js/model.js
+++ js/model.js
@@ -1,20 +1,20 @@
 app.factory("myFactory", ($http, $q) => {
-  var object = {
+    var object = {
 
-    get() {
-      var pr = $q.defer();
-      var url = "https://api.coinmarketcap.com/v1/ticker/";
-      $http.get(url).then(function(data) {
-        pr.resolve(data.data);
+        get() {
+            var pr = $q.defer();
+            var url = "https://api.coinmarketcap.com/v1/ticker/";
+            $http.get(url).then(function (data) {
+                pr.resolve(data.data);
 
-      }, function(err) {
-        pr.reject(err);
-      });
-      return pr.promise;
+            }, function (err) {
+                pr.reject(err);
+            });
+            return pr.promise;
+        }
+
+
     }
 
-
-  }
-
-  return object;
+    return object;
 });
--- js/route.js
+++ js/route.js
@@ -1,40 +1,40 @@
-app.config(function($routeProvider, $locationProvider) {
+app.config(function ($routeProvider,$locationProvider){
   $locationProvider.hashPrefix('')
   $routeProvider
-    .when("/home", {
-      templateUrl: "index.html"
-    })
-    .when("/bitcoin", {
-      templateUrl: "pages/bitcoin.html"
-    })
-    .when("/etherium", {
-      templateUrl: "pages/etherium.html"
-    })
-    .when("/ripple", {
-      templateUrl: "pages/ripple.html"
-    })
-    .when("/bitcoinCash", {
-      templateUrl: "pages/bitcoinCash.html"
-    })
-    .when("/eos", {
-      templateUrl: "pages/eos.html"
-    })
-    .when("/litecoin", {
-      templateUrl: "pages/litecoin.html"
-    })
-    .when("/cardano", {
-      templateUrl: "pages/cardano.html"
-    })
-    .when("/stellar", {
-      templateUrl: "pages/stellar.html"
-    })
-    .when("/iota", {
-      templateUrl: "pages/iota.html"
-    })
-    .when("/tron", {
-      templateUrl: "pages/tron.html"
-    })
-    .otherwise({
-      redirectTO: "/index.html"
-    })
+  .when("/home",{
+    templateUrl:"index.html"
+  })
+  .when("/bitcoin",{
+    templateUrl:"pages/bitcoin.html"
+  })
+  .when("/etherium",{
+    templateUrl:"pages/etherium.html"
+  })
+  .when("/ripple",{
+    templateUrl:"pages/ripple.html"
+  })
+  .when("/bitcoinCash",{
+    templateUrl:"pages/bitcoinCash.html"
+  })
+  .when("/eos",{
+    templateUrl:"pages/eos.html"
+  })
+  .when("/litecoin",{
+    templateUrl:"pages/litecoin.html"
+  })
+  .when("/cardano",{
+    templateUrl:"pages/cardano.html"
+  })
+  .when("/stellar",{
+    templateUrl:"pages/stellar.html"
+  })
+  .when("/iota",{
+    templateUrl:"pages/iota.html"
+  })
+  .when("/tron",{
+    templateUrl:"pages/tron.html"
+  })
+  .otherwise({
+    redirectTO:"/index.html"
+  })
 })
